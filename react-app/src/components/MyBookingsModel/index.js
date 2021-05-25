85d6da1ee737a5381193d2468be9270ea7c249e9
adambailey <arb5433@gmail.com>
2021-05-25 16:55:11 -0400

    “.”

--- react-app/src/components/MyBookingsModel/index.js
+++ react-app/src/components/MyBookingsModel/index.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
+import {useHistory} from 'react-router-dom';
 import { Modal } from "../../context/Modal";
 import MyBooking from "./MyBooking";
 
 function MyBookingModal() {
   const [showModal, setShowModal] = useState(false);
+  const history = useHistory()
+  const onClick = () => {
+    history.push('/')
+    setShowModal(true)
+  }
 
   return (
     <>
-      <button className='edt-and-del-btns' onClick={() => setShowModal(true)}>
+      <button className='edt-and-del-btns' onClick={onClick}>
         Bookings |
       </button>
       {showModal && (
